384f1e65bf2906d74be14044d4727bb9c6caf50e
JHG@LT <rpg2657@gmail.com>
2024-10-02 12:16:59 +0900

    정수를 랜덤하케 설정하는 함수 생성

--- boy_follows_an_arrow_of_hand.py
+++ boy_follows_an_arrow_of_hand.py
@@ -1,6 +1,15 @@
 from pico2d import *
+import random
 
 open_canvas()
 
+def random_locate():
+    global x, y
+
+    x, y = random.randint(0, 800), random.randint(0, 600)
+
+x, y = 0, 0
+random_locate()
+print(x, y)
 
 close_canvas()
