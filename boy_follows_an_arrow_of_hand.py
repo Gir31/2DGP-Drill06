3a3ce4c3e8c43bb4b5de45786c9735e809fe65e7
JHG@LT <rpg2657@gmail.com>
2024-10-02 14:17:27 +0900

    캐릭터 랜덤위치로 움직이는 애니메이션 구현

--- boy_follows_an_arrow_of_hand.py
+++ boy_follows_an_arrow_of_hand.py
@@ -2,21 +2,40 @@ from pico2d import *
 import random
 
 open_canvas()
+character = load_image('run_animation.png')
+point = load_image('hand_arrow.png')
 
 def random_locate():
-    global x, y
+    global base_x, base_y, des_x, des_y
+    base_x , base_y = des_x, des_y
 
-    x, y = random.randint(0, 800), random.randint(0, 600)
+    des_x, des_y = random.randint(0,800), random.randint(0, 600)
 
-character = load_image('run_animation.png')
-x, y, frame = 0, 0, 0
+    pass
+
+def character_move_animation(p1, p2):
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+    frame = 0
+
+    random_locate()
+
+    for i in range(0, 100 + 1, 4):
+        clear_canvas()
+        t = i / 100
+        x = (1 - t) * x1 + t * x2
+        y = (1 - t) * y1 + t * y2
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+    pass
+
+base_x, base_y, des_x, des_y = 0, 0, 0, 0
 
 while True:
-    clear_canvas()
-    random_locate()
-    character.clip_draw(frame*100, 0, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
-    delay(0.5)
+
+    character_move_animation((base_x, base_y), (des_x, des_y))
 
 close_canvas()
